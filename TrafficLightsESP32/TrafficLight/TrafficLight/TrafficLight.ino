adc7519229d4cc17644ce8ce4952e92cd6390d20
Usin2705 <Usin2705@yahoo.com>
2019-06-01 22:23:16 +0300

    feat: a working traffic light controller with offline mode

--- TrafficLightsESP32/TrafficLight/TrafficLight/TrafficLight.ino
+++ TrafficLightsESP32/TrafficLight/TrafficLight/TrafficLight.ino
@@ -15,8 +15,11 @@
 EventGroupHandle_t tasksEventGrp;
 #define BIT_0	( 1 << 0 ) //No wifi (unblock wifi initialize task)
 #define BIT_1	( 1 << 1 ) //No mqtt (unblock mqtt initialize task)
+#define BIT_2	( 1 << 2 ) //No server (unblock self running task)
+
 #define CHECK_WIFI_FLAG BIT_0
 #define CHECK_MQTT_FLAG BIT_1
+#define SELF_RUN_FLAG BIT_2
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
@@ -25,8 +28,27 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2);
 WiFiClient espClient; //pubsubclient
 PubSubClient mqttClient(espClient);
 
+const int redPin = GPIO_NUM_32;
+const int yelPin = GPIO_NUM_33;
+const int grePin = GPIO_NUM_25;
 
-const char* MQTT_TOPIC = "TRAFFIC1";
+const int SERVER_TIMEOUT = 60;	//Default  = 60
+
+const int RED_STATE = 0;
+const int YELLOW_STATE = 1;
+const int GREEN_STATE = 2;
+const int ALL_STATE = 3;
+const int NONE_STATE = 4;
+
+const char* MQTT_TOPIC = "TRAFFIC1"; //Replace this one for the traffic light number
+
+int isFirstTime = true; //False
+
+
+//BUG: AFTER A LONG TIME OF SELF RUN, LCD STOP WORKING (NOT PRINT OUT ANYTHING) (MAYBE THE LCD IS BROKEN SOMEHOW)
+//PROBABLY DUE TO WRITING TO LCD WAS INTERUPTED BY OTHER TASK WRITING TO THE SAME LCD
+//SHOULD BLOCK THE LCD WRITING WITH SOME MUTEX??
+// TODO: ADD MUTEX TO BLOCK WHEN USING LCD
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -35,9 +57,19 @@ void setup() {
 	lcd.begin(GPIO_NUM_21, GPIO_NUM_22);	
 	lcd.backlight();
 	lcd.clear();
+
+	// setup digital output pin
+	pinMode(redPin, OUTPUT);
+	pinMode(yelPin, OUTPUT);
+	pinMode(grePin, OUTPUT);
+
 	tasksEventGrp = xEventGroupCreate();
 	xTaskCreate(wifiInit, "wifiInit", 7000, NULL, 1, NULL);
 	xTaskCreate(mqttTask, "mqttTask", 7000, NULL, 1, NULL);
+	xTaskCreate(selfService, "selfService", 7000, NULL, 1, NULL);
+	/* Start the scheduler */
+	//We don't need this to start the tasks :-/
+	//vTaskStartScheduler();
 }
 
 // the loop function runs over and over again until power down or reset
@@ -95,6 +127,7 @@ void mqttTask(void* parameter) {
 	mqttClient.setServer(mqtt_server, 1883);
 	mqttClient.setCallback(mqttCallback);
 
+	TickType_t lastTime = xTaskGetTickCount();
 	while (1) {
 		//If not connect to MQTT
 		if (!mqttClient.loop()) {
@@ -102,7 +135,69 @@ void mqttTask(void* parameter) {
 			xEventGroupSetBits(tasksEventGrp, CHECK_WIFI_FLAG);
 			//Reconnect to MQTT
 			reconnect();
+			
+			//If MQTT is down for more than 60s
+			if (((xTaskGetTickCount() - lastTime) / xPortGetTickRateHz()) >= SERVER_TIMEOUT) {
+				xEventGroupSetBits(tasksEventGrp, SELF_RUN_FLAG);
+			}
 		}
+
+		else {			
+			lastTime = xTaskGetTickCount();
+			xEventGroupClearBits(tasksEventGrp, SELF_RUN_FLAG);
+		}
+	}
+}
+
+void selfService(void* parameter) {
+	TickType_t lastTime = xTaskGetTickCount();
+
+	int state = YELLOW_STATE;
+	int timeLapse = 0;
+	int count = 0;
+	int increment = -1;
+	while (1) {
+		//waiting until self run (server are down) flag are set
+		xEventGroupWaitBits(tasksEventGrp, SELF_RUN_FLAG, pdFALSE, pdTRUE, portMAX_DELAY);
+		
+		if (isFirstTime) {
+			turnLightOn(ALL_STATE);
+			vTaskDelay(pdMS_TO_TICKS(500));
+			turnLightOn(NONE_STATE);
+			vTaskDelay(pdMS_TO_TICKS(500));
+			turnLightOn(ALL_STATE);
+			vTaskDelay(pdMS_TO_TICKS(500));
+			turnLightOn(NONE_STATE);
+
+			lastTime = xTaskGetTickCount();
+			state = YELLOW_STATE;
+			timeLapse = 0;
+			count = 0;
+			increment = -1;			
+
+			isFirstTime = false;
+		}
+
+		if (((xTaskGetTickCount() - lastTime) / xPortGetTickRateHz()) >= timeLapse) {
+			count = 0;
+			lcd.clear();
+			lcd.setCursor(0, 0);
+			lcd.print("SERVER ERROR!");
+
+			state = state + increment;
+
+			//Swith the increment sign (to make sure it go R-Y-G-Y-R
+			if ((state == GREEN_STATE) || (state == RED_STATE)) increment = increment*-1;
+			turnLightOn(state);
+			lastTime = xTaskGetTickCount();
+		}
+
+		timeLapse = state == 1 ? 4 : 20;
+		count++;		
+
+		lcd.setCursor(14, 0);
+		lcd.printf("%02d", timeLapse - count / 2);
+		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
 
@@ -121,13 +216,26 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
 	char buf[MQTT_MAX_PACKET_SIZE + 1 + 9];
 	sprintf(buf, "%s %s", MQTT_TOPIC, payloadstring);
 
-
 	mqttClient.publish("Status", buf);
 	lcd.clear();
 	lcd.setCursor(0, 0);
 	lcd.print("MQTT");
 	lcd.setCursor(0, 1);
 	lcd.print(payloadstring);
+	char red[] = "RED";
+	char yellow[] = "YELLOW";
+	char green[] = "GREEN";
+	
+	if (memcmp(payloadstring, red, sizeof(red))==0) {
+		turnLightOn(RED_STATE);
+	}
+	else if (memcmp(payloadstring, yellow, sizeof(yellow)) == 0) {
+		turnLightOn(YELLOW_STATE);
+
+	} 
+	else if (memcmp(payloadstring, green, sizeof(green)) == 0) {
+		turnLightOn(GREEN_STATE);
+	}
 }
 
 void reconnect() {
@@ -135,10 +243,12 @@ void reconnect() {
 	//If Check MQTT flag is set mean: wifi is connected
 	xEventGroupWaitBits(tasksEventGrp, CHECK_MQTT_FLAG, pdFALSE, pdTRUE, portMAX_DELAY);
 	if (!mqttClient.connected()) {
+		/*
 		lcd.setCursor(0, 0);
 		lcd.print("MQTT");
 		lcd.setCursor(0, 1);
 		lcd.print("Connecting...");
+		*/
 		Serial.print("Attempting MQTT connection...");
 		// Create a random mqttClient ID
 		String clientId = "ESP8266Client-";
@@ -153,6 +263,8 @@ void reconnect() {
 			//Subscribe to topic
 			mqttClient.subscribe(MQTT_TOPIC);
 			xEventGroupClearBits(tasksEventGrp, CHECK_MQTT_FLAG);
+			xEventGroupClearBits(tasksEventGrp, SELF_RUN_FLAG);
+			isFirstTime = true;
 		}
 		else {
 			Serial.print("failed, rc=");
@@ -161,4 +273,41 @@ void reconnect() {
 
 	}
 
+}
+
+void turnLightOn(int lightNo) {
+
+	switch (lightNo) {
+	
+	case RED_STATE:
+
+		digitalWrite(redPin, HIGH);
+		digitalWrite(yelPin, LOW);
+		digitalWrite(grePin, LOW);
+		break;
+
+	case YELLOW_STATE:
+		digitalWrite(redPin, LOW);
+		digitalWrite(yelPin, HIGH);
+		digitalWrite(grePin, LOW);
+		break;
+
+	case GREEN_STATE:
+		digitalWrite(redPin, LOW);
+		digitalWrite(yelPin, LOW);
+		digitalWrite(grePin, HIGH);
+		break;
+
+	case ALL_STATE:
+		digitalWrite(redPin, HIGH);
+		digitalWrite(yelPin, HIGH);
+		digitalWrite(grePin, HIGH);
+		break;
+
+	default:
+		digitalWrite(redPin, LOW);
+		digitalWrite(yelPin, LOW);
+		digitalWrite(grePin, LOW);
+		break;
+	}
 }
--- TrafficLightsESP32/TrafficLight/TrafficLight/Utils.h
+++ TrafficLightsESP32/TrafficLight/TrafficLight/Utils.h
@@ -1,54 +1,18 @@
 // Utils.h
+#ifndef _UTILS_h
+#define _UTILS_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+#endif
 
 #include <Arduino.h>
-#include <HTTPClient.h>
-#include <ArduinoOTA.h>
-#include <EEPROM.h>
+#include <ArduinoOTA.h> //the setup time function wont work without this
 
-const char* test_root_ca = \
-"-----BEGIN CERTIFICATE-----\n"
-"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n"
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
-"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n"
-"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n"
-"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n"
-"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n"
-"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n"
-"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n"
-"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n"
-"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n"
-"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n"
-"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n"
-"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n"
-"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n"
-"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n"
-"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n"
-"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n"
-"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n"
-"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n"
-"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n"
-"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n"
-"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n"
-"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n"
-"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n"
-"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n"
-"-----END CERTIFICATE-----";
 
-//Not really do anything ... :-/
-//https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino
-void setupTime() {
-	//const char* TZ_INFO = "EST5EDT4,M3.2.0/02:00:00,M11.1.0/02:00:00";
-	//esp8266 example https://github.com/esp8266/Arduino/blob/master/libraries/esp8266/examples/NTP-TZ-DST/NTP-TZ-DST.ino
-	//https://github.com/espressif/arduino-esp32/issues/1114
-	//https://github.com/espressif/arduino-esp32/issues/403
-	//https://github.com/espressif/esp-idf/tree/master/examples/protocols/sntp important
-	///usr/share/zoneinfo/Europe get time zone config by cat file in this directory, ubuntu
-	struct tm timeinfo;
-	configTzTime("EET-2EEST,M3.5.0/3,M10.5.0/4", "time.nist.gov", "0.pool.ntp.org", "1.pool.ntp.org");
-	if (getLocalTime(&timeinfo, 10000)) {  // wait up to 10sec to sync
-		Serial.println(&timeinfo, "Time set: %B %d %Y %H:%M:%S (%A)");
-	}
-	else {
-		Serial.println("Time not set");
-	}
-}
+void setupTime();
